aa6a325a375db8c712b6c0cc4d8e7ae8e5e80c0f
nastya <milovanova-aa@yandex.ru>
2024-09-20 16:06:35 +0300

    Лабораторная №1

---
+++ students/23K0164/23K0164-p01/src/main/java/ru/mirea/practice/s23k0164/t2/Four.java
@@ -0,0 +1,47 @@
+package ru.mirea.practice.s23k0164.t2;
+
+import java.util.Scanner;
+
+public final class Four {
+
+    private Four() {
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите размер массива:");
+            int size = scanner.nextInt();
+            int[] arr = new int[size];
+
+            for (int i = 0; i < arr.length; i++) {
+                System.out.printf("Введите элемент %d: ", i + 1);
+                arr[i] = scanner.nextInt();
+            }
+
+            int min = arr[0];
+            int max = arr[0];
+            int sum = 0;
+            int i = 0;
+
+            while (i < arr.length) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                sum += arr[i];
+                i++;
+            }
+
+            System.out.println("\nМассив: ");
+            for (int elem : arr) {
+                System.out.print(elem + " ");
+            }
+            System.out.println("\nМинимальное значение: " + min);
+            System.out.println("Максимальное значение: " + max);
+            System.out.println("Сумма элементов: " + sum);
+        }
+    }
+
+}
