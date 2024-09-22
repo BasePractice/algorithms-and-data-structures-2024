a03a69905d604108086474a4e8d7ba88ed606519
AndreySkrypov <a.skrypov2005@gmail.com>
2024-09-22 22:16:27 +0300

    Лабораторная №1

--- students/23K0368/23K0368-p01/src/main/java/ru/mirea/practice/s0000001/program5.java
+++ students/23K0368/23K0368-p01/src/main/java/ru/mirea/practice/s0000001/program5.java
@@ -1,17 +1,11 @@
 package ru.mirea.practice.s0000001;
+import java.util.Arrays;
 
 public class program5 {
     public static void main(String[] args) {
-        // Проверка, если аргументы переданы
-        if (args.length > 0) {
-            System.out.println("Аргументы командной строки:");
-
-            // Цикл for для вывода каждого аргумента
             for (String arg : args) {
                 System.out.println(arg);
             }
-        } else {
-            System.out.println("Аргументы командной строки отсутствуют.");
-        }
+            System.out.println(Arrays.toString(args));
     }
 }
