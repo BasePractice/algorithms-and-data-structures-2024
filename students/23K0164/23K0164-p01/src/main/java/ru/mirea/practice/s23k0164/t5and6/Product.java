e27e444590dc90215676d75b53c1f08272c6da7c
nastya <milovanova-aa@yandex.ru>
2024-09-22 21:46:31 +0300

    Лабораторная №1

---
+++ students/23K0164/23K0164-p01/src/main/java/ru/mirea/practice/s23k0164/t5and6/Product.java
@@ -0,0 +1,27 @@
+package ru.mirea.practice.s23k0164.t5and6;
+
+public class Product {
+    public enum ListProduct {
+        Product1("Умные наушники \"SoundWave Pro\"", 13688),
+        Product2("Эко-сумка \"GreenBag\"", 1824),
+        Product3("Косметический набор \"GlowUp\"",3649),
+        Product4("Игровая консоль \"GameSphere X\"",45631);
+
+        private final String name;
+        private final int cost;
+
+
+        ListProduct(String name, int cost) {
+            this.name = name;
+            this.cost = cost;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getCost() {
+            return cost;
+        }
+    }
+}
