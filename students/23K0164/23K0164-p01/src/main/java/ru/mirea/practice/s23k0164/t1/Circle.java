9262dd131a9d0233d97891f55094d6d3ffa55d27
nastya <milovanova-aa@yandex.ru>
2024-09-23 00:16:26 +0300

    Лабораторная №1

---
+++ students/23K0164/23K0164-p01/src/main/java/ru/mirea/practice/s23k0164/t1/Circle.java
@@ -0,0 +1,27 @@
+package ru.mirea.practice.s23k0164.t1;
+
+public class Circle extends Share {
+    private double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String getType() {
+        return "Circle";
+    }
+
+    @Override
+    public double getArea() {
+        return radius * Math.PI * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (radius * Math.PI);
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", Радиус: " + radius;
+    }
+}
