302501f0d32b6ae24a63d8e0ceba770ca97874ae
nastya <milovanova-aa@yandex.ru>
2024-09-22 18:35:32 +0300

    Лабораторная №1

---
+++ students/23K0164/23K0164-p01/src/main/java/ru/mirea/practice/s23k0164/t5/Seven.java
@@ -0,0 +1,23 @@
+package ru.mirea.practice.s23k0164.t5;
+
+public final class Seven {
+    private Seven() {
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Факториал определен только для неотрицательных чисел");
+        }
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        long result = calculateFactorial(number);
+        System.out.println("Факториал числа " + number + " равен " + result);
+    }
+}
