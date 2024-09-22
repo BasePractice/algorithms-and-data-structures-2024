84205e370c98c62c83d511c3f5ad95d686211c43
nastya <milovanova-aa@yandex.ru>
2024-09-22 23:56:28 +0300

    Лабораторная №1

--- students/23K0164/23K0164-p01/src/main/java/ru/mirea/practice/s23k0164/t2/MenClothing.java
+++
@@ -1,5 +0,0 @@
-package ru.mirea.practice.s23k0164.t2;
-
-public interface MenClothing {
-    void dressMan();
-}
--- students/23K0164/23K0164-p01/src/main/java/ru/mirea/practice/s23k0164/t2/WomenClothing.java
+++
@@ -1,5 +0,0 @@
-package ru.mirea.practice.s23k0164.t2;
-
-public interface WomenClothing {
-    void dressWomen();
-}
