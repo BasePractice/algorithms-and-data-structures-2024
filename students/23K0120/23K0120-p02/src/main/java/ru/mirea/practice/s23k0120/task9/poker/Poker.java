8972471eae9cc8ae149e10796abf436379ec388e
I_like_Space <dandyl.dmitry@mail.ru>
2024-09-22 14:12:05 +0300

    Лабораторная №2

---
+++ students/23K0120/23K0120-p02/src/main/java/ru/mirea/practice/s23k0120/task9/poker/Poker.java
@@ -0,0 +1,34 @@
+package ru.mirea.practice.s23k0120.task9.poker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public abstract class Poker {
+    public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<>();
+        Deck deck = new Deck();
+        Collections.shuffle(deck);
+
+        //System.out.println(deck);
+
+        System.out.print("Enter number of players: ");
+        int numOfPlayers = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+
+            numOfPlayers = sc.nextInt();
+        }
+        for (int i = 0; i < numOfPlayers; i++) {
+            players.add(new Player());
+        }
+
+        for (int i = 0; i < 5 * numOfPlayers; i++) {
+            players.get(i % numOfPlayers).add(deck.removeLast());
+        }
+
+        for (int i = 0; i < numOfPlayers; i++) {
+            System.out.printf("Player №%d: %s\n", i, players.get(i));
+            System.out.print("\n");
+        }
+    }
+}
