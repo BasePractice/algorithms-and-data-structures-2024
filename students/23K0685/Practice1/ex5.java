0d2cbc9a5bb88724dedcc6a06af9c9be6e4af052
lev_d <leo.dolgushine@gmail.com>
2024-09-23 20:27:56 +0300

    HomePractice1

---
+++ students/23K0685/Practice1/ex5.java
@@ -0,0 +1,10 @@
+package ru.mirea.task1;
+
+public class ex5 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
---
+++ students/23K0685/Practice1/ex7.java
@@ -0,0 +1,16 @@
+package ru.mirea.task1;
+
+public class ex7 {
+    public int Factorial(int strt) {
+        int Res = 1;
+        for (int i = 1; i < strt+1;i++) {
+            Res = Res * i;
+        }
+        return Res;
+    }
+    public static void main(String[] args) {
+        ex7 gd = new ex7();
+        System.out.println(gd.Factorial(5));
+    }
+}
+
