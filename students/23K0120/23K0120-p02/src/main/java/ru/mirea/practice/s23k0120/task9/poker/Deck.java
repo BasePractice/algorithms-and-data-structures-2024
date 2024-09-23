782dbef9ba7cbdb44a147f89397a640bf6ce7de6
I_like_Space <dandyl.dmitry@mail.ru>
2024-09-23 20:35:33 +0300

    Лабораторная №2

---
+++ students/23K0120/23K0120-p02/src/main/java/ru/mirea/practice/s23k0120/task9/poker/Deck.java
@@ -0,0 +1,18 @@
+package ru.mirea.practice.s23k0120.task9.poker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Deck extends ArrayList<Card> {
+    public Deck() {
+        for (Card.Suits suit: Card.Suits.values()) {
+            for (Card.Ranks rank: Card.Ranks.values()) {
+                this.add(new Card(suit, rank));
+            }
+        }
+        this.add(new Card(Card.Suits.HEARTS, Card.Ranks.JOKER));
+        this.add(new Card(Card.Suits.SPADES, Card.Ranks.JOKER));
+    }
+
+
+}
