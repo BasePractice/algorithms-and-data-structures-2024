3156fba876fd788203b9df71899c9e83cd2760f8
I_like_Space <dandyl.dmitry@mail.ru>
2024-09-23 17:16:06 +0300

    Лабораторная №3

---
+++ students/23K0120/23K0120-p03/src/main/java/ru/mirea/practice/s23k0120/formatting/report/Employee.java
@@ -0,0 +1,32 @@
+package ru.mirea.practice.s23k0120.formatting.report;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Employee {
+    private String fullName;
+    private float salary;
+    public Employee(String fullName, float salary) {
+        this.fullName = fullName;
+        this.salary = salary;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        String salary = format.format(getSalary());
+        return String.format("Full name: %20s \t\t\t\t\tSalary: %20s", getFullName(), salary);
+    }
+}
