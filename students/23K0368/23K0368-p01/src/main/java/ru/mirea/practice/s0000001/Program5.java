401a997add61d43ec642cd4617c60177706792d1
AndreySkrypov <a.skrypov2005@gmail.com>
2024-09-23 22:27:36 +0300

    Лабораторная №1

--- students/23K0368/23K0368-p01/src/main/java/ru/mirea/practice/s0000001/Program5.java
+++ students/23K0368/23K0368-p01/src/main/java/ru/mirea/practice/s0000001/Program5.java
@@ -1,7 +1,7 @@
 package ru.mirea.practice.s0000001;
 import java.util.Arrays;
 
-public class program5 {
+public abstract class Program5 {
     public static void main(String[] args) {
             for (String arg : args) {
                 System.out.println(arg);
