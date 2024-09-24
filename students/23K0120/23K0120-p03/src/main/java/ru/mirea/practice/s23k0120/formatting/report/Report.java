1c14aeb19f49b81517e592018259ee51c920ee42
I_like_Space <dandyl.dmitry@mail.ru>
2024-09-24 09:18:10 +0300

    Лабораторная №3 (Вер. 1.1)

--- students/23K0120/23K0120-p03/src/main/java/ru/mirea/practice/s23k0120/formatting/report/Report.java
+++ students/23K0120/23K0120-p03/src/main/java/ru/mirea/practice/s23k0120/formatting/report/Report.java
@@ -14,7 +14,7 @@ public abstract class Report {
     public static void main(String[] args) {
         Random random = new Random();
         for (int i = 0; i < employees.length; i++) {
-            employees[i] = new Employee("John Smith", random.nextFloat(5000, 500000));
+            employees[i] = new Employee("John Smith", random.nextFloat(5000f, 50000f));
         }
 
         generateReport();
