80e6327e42270b7fc866ac4652f7727ec85e784d
Alex <side.salt1955@gmail.com>
2024-09-24 18:31:38 +0300

    Лабараторная работа №1

--- students/23K0365/23K0365-p01/src/main/java/ru/mirea/practice/s0000001/Prog3.java
+++ students/23K0365/23K0365-p01/src/main/java/ru/mirea/practice/s0000001/Prog3.java
@@ -2,14 +2,14 @@ package ru.mirea.practice.s0000001;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class prog3 {
+public abstract class Prog3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int ln = 3, sr =0, sum=0;
+        try (Scanner sc = new Scanner(System.in)){
+        int ln = 3;
         System.out.println("lenght of array = " + ln);
         int [] b = new int [ln];
         for (int i=0;i<ln;i++){b[i]=sc.nextInt();}
-        sum = Arrays.stream(b).sum();
-        sr=sum/ln;
+        int sum = Arrays.stream(b).sum();
+        int sr=sum/ln;
         System.out.println("summary: "+sum+"; srednee: "+sr);
-    }}
+    }}}
