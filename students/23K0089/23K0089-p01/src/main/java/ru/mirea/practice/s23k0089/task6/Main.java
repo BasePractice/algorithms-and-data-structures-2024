67b5112900f9073853cbb3fbdc4a80f927f1acd9
Akay <akay.v.k@edu.mirea.ru>
2024-09-25 21:43:53 +0300

    Лабораторная №1

---
+++ students/23K0089/23K0089-p01/src/main/java/ru/mirea/practice/s23k0089/task6/Main.java
@@ -0,0 +1,16 @@
+package ru.mirea.practice.s23k0089.task6;
+
+public abstract class Main{
+    public static void main(String[] args){
+        float an = 0;
+        for (int i=1; i<11; i++)
+        {
+            an += 1/(float)i;
+            for (int j=1; j<i; j++)
+            {
+                System.out.printf("1/%d + ", j);
+            }
+            System.out.printf("1/%d = %f\n", i, an);
+        }
+    }
+}
