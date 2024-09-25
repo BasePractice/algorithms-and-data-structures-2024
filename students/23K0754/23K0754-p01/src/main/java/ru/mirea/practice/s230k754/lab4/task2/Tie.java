5c3df88adc9baa9a232c6e58d915e4730270a32d
Andrey Nazarov <andrexnazarov@gmail.com>
2024-09-25 22:58:23 +0300

    добавил папку 24K0458 и лабораторную 4

---
+++ students/23K0754/23K0754-p01/src/main/java/ru/mirea/practice/s230k754/lab4/task2/Tie.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s230k754.lab4.task2;
+
+public class Tie extends Clothes implements MenClothing {
+    public Tie(double price, int size, String color){
+        super(price, size, color);
+    }
+
+    @Override
+    public void dressMan() {
+        System.out.printf("Мужской галстук: цена: %f,цвет: %s,размер: %d\n", this.getPrice(), this.getColor(), this.getSize());
+    }
+}
