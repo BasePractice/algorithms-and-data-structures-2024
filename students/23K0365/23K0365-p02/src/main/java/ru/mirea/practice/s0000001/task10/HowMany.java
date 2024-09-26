49414517a7d305086348c86cd107c5bde3923ad5
Alex <side.salt1955@gmail.com>
2024-09-26 14:58:23 +0300

    Лабараторная работа №1

---
+++ students/23K0365/23K0365-p02/src/main/java/ru/mirea/practice/s0000001/task10/HowMany.java
@@ -0,0 +1,17 @@
+package ru.mirea.practice.s0000001.task10;
+import java.util.Scanner;
+
+public abstract class HowMany {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+        System.out.println("Sentence:");
+
+        String line = sc.nextLine();
+
+        if(line != null){
+            String[] words = line.split("\\s+");
+            System.out.println(words.length);
+        }
+        else{System.out.println(0);}
+    }
+}}
