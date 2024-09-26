3efcfb6d21a0a83949c7a435901005d5c04cef9d
Alex <side.salt1955@gmail.com>
2024-09-26 19:12:02 +0300

    Лабараторная работа №1

--- students/23K0365/23K0365-p02/src/main/java/ru/mirea/practice/s0000001/task1/Author.java
+++ students/23K0365/23K0365-p02/src/main/java/ru/mirea/practice/s0000001/task1/Author.java
@@ -1,8 +1,10 @@
 package ru.mirea.practice.s0000001.task1;
+
 public class Author {
     private String name;
     private String email;
     private char gender;
+
     public Author(String name, String email, char gender){
         this.name=name;
         this.email=email;
@@ -11,11 +13,13 @@ public class Author {
 
     public String getName() {return name;}
     public void setEmail(String email){this.email=email;}
+
     @Override
     public String toString(){
-        if(gender=='m'){
-        return "Author is "+name+ ". He's email is "
-                +email+". He is a man";}
-        else{return "Author is "+name+ ". She's email is "
+        if(gender=='m') {
+            return "Author is "+name+ ". He's email is "
+                    +email+". He is a man";}
+        else {
+            return "Author is "+name+ ". She's email is "
                 +email+". She is a wOmAn";}}
 }
--- students/23K0365/23K0365-p02/src/main/java/ru/mirea/practice/s0000001/task9/Poker.java
+++ students/23K0365/23K0365-p02/src/main/java/ru/mirea/practice/s0000001/task9/Poker.java
@@ -6,30 +6,43 @@ import java.util.List;
 import java.util.Scanner;
 
 public abstract class Poker {
-    public static final String[] RANKS = {"2","3","4","5","King","Aces"};
-    public static final String[] SUITS = {"Spades","Diamond","Clubs","Heats"};
+    public static final String[] RANKS = {
+        "2",
+        "3",
+        "4",
+        "5",
+        "King",
+        "Aces"
+    };
+    public static final String[] SUITS = {
+        "Spades",
+        "Diamond",
+        "Clubs",
+        "Heats"
+    };
 
     public static void main(String[] args) {
-        try(Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
 
-        System.out.println("Amount of players:");
-        int amountPlayers = sc.nextInt();
+            System.out.println("Amount of players:");
+            int amountPlayers = sc.nextInt();
 
-        List<String> deck = new ArrayList<>();
-        for(String a : SUITS){
-            for(String b : RANKS){
-                deck.add(b +" of "+ a);
+            List<String> deck = new ArrayList<>();
+            for (String a: SUITS) {
+                for (String b: RANKS) {
+                    deck.add(b + " of " + a);
+                }
             }
-        }
-        Collections.shuffle(deck);
+            Collections.shuffle(deck);
 
-        for(int i=0;i<amountPlayers;i++){
-            System.out.print("Player #"+(i+1)+": ");
-            for(int j =0;j<5;j++) {
-                System.out.print(deck.remove(0)+", ");
+            for (int i = 0; i < amountPlayers; i++) {
+                System.out.print("Player #" + (i + 1) + ": ");
+                for (int j = 0; j < 5; j++) {
+                    System.out.print(deck.remove(0) + ", ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
 
+        }
     }
-}}
+}
