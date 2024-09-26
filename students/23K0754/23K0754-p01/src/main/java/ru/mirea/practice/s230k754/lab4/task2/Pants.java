b401168569b25b13524bbdf808a37eabf1852ee5
Andrey Nazarov <andrexnazarov@gmail.com>
2024-09-26 23:34:11 +0300

    лаб 4

---
+++ students/23K0754/23K0754-p01/src/main/java/ru/mirea/practice/s230k754/lab4/task2/Pants.java
@@ -0,0 +1,18 @@
+package ru.mirea.practice.s230k754.lab4.task2;
+
+public class Pants extends Clothes implements MenClothing,WomenClothing  {
+    public Pants(double price, int size, String color){
+        super(price, size, color);
+
+    }
+
+    @Override
+    public void dressMan() {
+        System.out.printf("Мужские штаны: цена: %f,цвет: %s,размер: %d\n", this.getPrice(), this.getColor(), this.getSize());
+    }
+
+    @Override
+    public void dressWomen() {
+        System.out.printf("Женские штаны: цена: %f,цвет: %s,размер: %d\n", this.getPrice(), this.getColor(), this.getSize());
+    }
+}
