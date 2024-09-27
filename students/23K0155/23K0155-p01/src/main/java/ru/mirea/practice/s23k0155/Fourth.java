6d6728b09e43fb9098b88127f4363a5f2e9fbbeb
l0lsi <lesnarik2005@gmail.com>
2024-09-27 15:02:46 +0300

    Лабораторная №1

--- students/23K0155/23K0155-p01/src/main/java/ru/mirea/practice/s23k0155/Fourth.java
+++ students/23K0155/23K0155-p01/src/main/java/ru/mirea/practice/s23k0155/Fourth.java
@@ -1,10 +1,10 @@
 package ru.mirea.practice.s23k0155;
 
-public class Fourth {
-    public static void main(String[] args){
+public abstract class Fourth {
+    public static void main(String[] args) {
         float t;
-        for(int i=1; i<11; i++){
-            t = 1/(float)i;
+        for (int i = 1; i < 11; i++) {
+            t = 1 / (float) i;
             //System.out.println("Number " + i + ": " + t);
             System.out.printf("Number %d: %.3f\n", i, t);
         }
