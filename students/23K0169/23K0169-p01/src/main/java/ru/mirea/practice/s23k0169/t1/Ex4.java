3bd30578c47249aa62d314fa67808427ba70a42d
SashaN2005g <aleksandraaa.nab@mail.ru>
2024-09-27 15:01:37 +0300

    Лабораторная №1

---
+++ students/23K0169/23K0169-p01/src/main/java/ru/mirea/practice/s23k0169/t1/Ex4.java
@@ -0,0 +1,36 @@
+package ru.mirea.practice.s23k0169.t1;
+
+import java.util.Scanner;
+
+public final class Ex4 {
+
+    private Ex4() {
+
+    }
+
+    public static void main(String[] args) {
+
+        try(Scanner sc = new Scanner(System.in)) {
+            int sum = 0;
+            int[] myArray = new int[6];
+            int i = 0;
+            int max = 0;
+            int min = 10000;
+
+            while (i < 6) {
+                myArray[i] = sc.nextInt();
+                if (myArray[i] > max) {
+                    max = myArray[i];
+                }
+                if (myArray[i] < min) {
+                    min = myArray[i];
+                }
+                sum += myArray[i];
+                i++;
+            }
+            System.out.println("Сумма всех элементов: " + sum);
+            System.out.println("Максимальный элемент: " + max);
+            System.out.println("Минимальный элемент: " + min);
+        }
+    }
+}
