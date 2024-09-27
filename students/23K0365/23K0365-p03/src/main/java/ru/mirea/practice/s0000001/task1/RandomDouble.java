b3f1ead11ddf5bb637f5875ebaeb15d8546832c0
Alex <side.salt1955@gmail.com>
2024-09-27 12:34:29 +0300

    Лабараторная работа №3

---
+++ students/23K0365/23K0365-p03/src/main/java/ru/mirea/practice/s0000001/task1/RandomDouble.java
@@ -0,0 +1,25 @@
+package prog1;
+import java.util.Random;
+public class RandomDouble {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println("First: ");
+        int lenAr=rand.nextInt(1,11);
+        System.out.println("Lenght of array is "+lenAr);
+
+        double[] arr1 = new double[lenAr];
+        for(int i =0;i<lenAr;i++){
+            arr1[i]=rand.nextDouble(10,20);
+            System.out.print(arr1[i]+"; ");
+    }
+        System.out.println();
+
+        System.out.println("Second:");
+        lenAr=(int) (Math.random()*10+1);
+        System.out.println("Lenght of array is "+lenAr);
+        double[] arr2 = new double[lenAr];
+        for(int i =0;i<lenAr;i++){
+            arr2[i]=(Math.random()*10);
+            System.out.print(arr2[i]+"; ");
+        }
+}}
