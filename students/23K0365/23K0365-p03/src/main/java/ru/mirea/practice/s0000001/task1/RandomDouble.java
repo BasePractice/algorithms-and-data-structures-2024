dee3eecdd217c9845f4933c4efb93a630c7d83cc
Alex <side.salt1955@gmail.com>
2024-09-28 19:53:12 +0300

    Лабараторная работа №3

--- students/23K0365/23K0365-p03/src/main/java/ru/mirea/practice/s0000001/task1/RandomDouble.java
+++ students/23K0365/23K0365-p03/src/main/java/ru/mirea/practice/s0000001/task1/RandomDouble.java
@@ -1,25 +1,28 @@
-package prog1;
+package ru.mirea.practice.s0000001.task1;
+
 import java.util.Random;
-public class RandomDouble {
+
+public abstract class RandomDouble {
     public static void main(String[] args) {
         Random rand = new Random();
         System.out.println("First: ");
-        int lenAr=rand.nextInt(1,11);
-        System.out.println("Lenght of array is "+lenAr);
+        int lenAr = rand.nextInt(1, 11);
+        System.out.println("Lenght of array is " + lenAr);
 
         double[] arr1 = new double[lenAr];
-        for(int i =0;i<lenAr;i++){
-            arr1[i]=rand.nextDouble(10,20);
-            System.out.print(arr1[i]+"; ");
-    }
+        for (int i = 0; i < lenAr; i++) {
+            arr1[i] = rand.nextDouble(10, 20);
+            System.out.print(arr1[i] + "; ");
+        }
         System.out.println();
 
         System.out.println("Second:");
-        lenAr=(int) (Math.random()*10+1);
-        System.out.println("Lenght of array is "+lenAr);
+        lenAr = (int)(Math.random() * 10 + 1);
+        System.out.println("Lenght of array is " + lenAr);
         double[] arr2 = new double[lenAr];
-        for(int i =0;i<lenAr;i++){
-            arr2[i]=(Math.random()*10);
-            System.out.print(arr2[i]+"; ");
+        for (int i = 0; i < lenAr; i++) {
+            arr2[i] = Math.random() * 10;
+            System.out.print(arr2[i] + "; ");
         }
-}}
+    }
+}
---
+++ students/23K0365/23K0365-p03/src/main/java/ru/mirea/practice/s0000001/task32/MagazineTest.java
@@ -0,0 +1,10 @@
+package ru.mirea.practice.s0000001.task32;
+
+public class MagazineTest {
+    public static void main(String[] args) {
+        Magazine arr = new Magazine(12.532, 99.999, 1234.5567);
+        int finPrice = 0;
+        finPrice += arr.addToU(arr);
+        arr.convertCurrency(finPrice);
+    }
+}
