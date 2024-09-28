cdc273f07f89411e2fa8846438a0fe57b6b8cedc
viktoriapermakova <gleb.permyakoff2011@gmail.com>
2024-09-28 22:25:41 +0300

    Лабораторная №2

---
+++ students/23K0364/23K0364-p02/src/main/java/ru/mirea/practice/s0000001/Circle.java
@@ -0,0 +1,19 @@
+package ru.mirea.practice.s0000001;
+
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
