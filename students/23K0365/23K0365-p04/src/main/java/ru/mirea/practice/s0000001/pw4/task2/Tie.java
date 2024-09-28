edeea57f4e828a0806b65dc4d803f5862eca0263
Alex <side.salt1955@gmail.com>
2024-09-28 20:46:27 +0300

    Лабараторная работа №3

---
+++ students/23K0365/23K0365-p04/src/main/java/ru/mirea/practice/s0000001/pw4/task2/Tie.java
@@ -0,0 +1,14 @@
+package ru.mirea.practice.s0000001.pw4.task2;
+
+public class Tie extends Clothes implements MenClothing {
+    public void dressMen(Clothes[] arr) {
+        System.out.println("Man gonna wear "
+                + getColor() + " Skirt(" + getSize()
+                + " euro size. " + getSize().getDescription() + "). It would cost "
+                + getPrice());
+    }
+
+    public Tie(Size size, int price, String color) {
+        super(size, price, color);
+    }
+}
