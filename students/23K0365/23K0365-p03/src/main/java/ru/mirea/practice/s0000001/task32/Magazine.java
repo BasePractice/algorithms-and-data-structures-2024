6eac81c0d364b7be198733438da9bd15b98cb779
Alex <side.salt1955@gmail.com>
2024-09-28 20:09:05 +0300

    Лабараторная работа №3

--- students/23K0365/23K0365-p03/src/main/java/ru/mirea/practice/s0000001/task32/Magazine.java
+++ students/23K0365/23K0365-p03/src/main/java/ru/mirea/practice/s0000001/task32/Magazine.java
@@ -11,7 +11,6 @@ public class Magazine {
     private double milkPrice;
     private double eggsPrice;
     private double fantePrice;
-    private Scanner sc = new Scanner(System.in);
 
     public Magazine(double milkPrice, double eggsPrice, double fantePrice) {
         this.milkPrice = milkPrice;
@@ -44,56 +43,60 @@ public class Magazine {
     }
 
     public double addToU(Magazine arr) {
-        double price = .0;
-        System.out.println("Next step:\n1.Add eggs\t2.Add Fanta\t3.Add Milk\t0.Exit");
-        int choose = sc.nextInt();
-        while (choose != 0) {
-            switch (choose) {
-                case 1:
-                    price += getEggsPrice();
-                    System.out.println("Eggs added!");
-                    break;
-                case 2:
-                    price += getFantePrice();
-                    System.out.println("Fanta added!");
-                    break;
-                case 3:
-                    price += getMilkPrice();
-                    System.out.println("Milk added!");
-                    break;
-                default:
-                    System.out.println("Wrong enter. Try again.");
-                    break;
-            }
+        try (Scanner sc = new Scanner(System.in)) {
+            double price = .0;
             System.out.println("Next step:\n1.Add eggs\t2.Add Fanta\t3.Add Milk\t0.Exit");
-            choose = sc.nextInt();
+            int choose = sc.nextInt();
+            while (choose != 0) {
+                switch (choose) {
+                    case 1:
+                        price += getEggsPrice();
+                        System.out.println("Eggs added!");
+                        break;
+                    case 2:
+                        price += getFantePrice();
+                        System.out.println("Fanta added!");
+                        break;
+                    case 3:
+                        price += getMilkPrice();
+                        System.out.println("Milk added!");
+                        break;
+                    default:
+                        System.out.println("Wrong enter. Try again.");
+                        break;
+                }
+                System.out.println("Next step:\n1.Add eggs\t2.Add Fanta\t3.Add Milk\t0.Exit");
+                choose = sc.nextInt();
+            }
+            return price;
         }
-        return price;
     }
 
     public void convertCurrency(double price) {
-        System.out.println("Choose currency:\n1.USD(default)\t2.EURO\t3.YUAN");
-        int choose = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Choose currency:\n1.USD(default)\t2.EURO\t3.YUAN");
+            int choose = sc.nextInt();
 
-        switch (choose) {
-            case 1:
-                NumberFormat format1 = NumberFormat.getCurrencyInstance(Locale.US);
-                System.out.println("\nU need to pay "
-                        + format1.format((float)(price / 99.9)));
-                break;
-            case 2:
-                NumberFormat format2 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-                System.out.println("\nU need to pay "
-                        + format2.format((float)(price / 97.8)));
-                break;
-            case 3:
-                NumberFormat format3 = NumberFormat.getCurrencyInstance(Locale.CHINA);
-                System.out.println("\nU need to pay "
-                        + format3.format((float)(price / 12.4)));
-                break;
-            default:
-                System.out.println("!TRY AGAIN!");
-                break;
+            switch (choose) {
+                case 1:
+                    NumberFormat format1 = NumberFormat.getCurrencyInstance(Locale.US);
+                    System.out.println("\nU need to pay "
+                            + format1.format((float)(price / 99.9)));
+                    break;
+                case 2:
+                    NumberFormat format2 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+                    System.out.println("\nU need to pay "
+                            + format2.format((float)(price / 97.8)));
+                    break;
+                case 3:
+                    NumberFormat format3 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+                    System.out.println("\nU need to pay "
+                            + format3.format((float)(price / 12.4)));
+                    break;
+                default:
+                    System.out.println("!TRY AGAIN!");
+                    break;
+            }
         }
     }
 }
