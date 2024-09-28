c638037c61566a1ab62ac89dab2c861277912d73
Alex <side.salt1955@gmail.com>
2024-09-28 23:26:17 +0300

    Лабараторная работа №4

---
+++ students/23K0365/23K0365-p04/src/main/java/ru/mirea/practice/s0000001/pw41/task6/Employer.java
@@ -0,0 +1,21 @@
+package ru.mirea.practice.s0000001.pw41.task6;
+
+public class Employer {
+    String fstName;
+    String scndName;
+    private int income;
+
+    public Employer(String fstName, String scndName, int income) {
+        this.fstName = fstName;
+        this.scndName = scndName;
+        this.income = income;
+    }
+
+    public int getIncome(int countOfDays) {
+        if (countOfDays >= 15) {
+            return income * 12;
+        } else {
+            return income;
+        }
+    }
+}
