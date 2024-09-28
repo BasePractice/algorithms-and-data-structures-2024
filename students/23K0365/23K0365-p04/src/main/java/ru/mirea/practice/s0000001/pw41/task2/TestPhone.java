1d1c7b090106eb35c91078858056e015f235eea8
Alex <side.salt1955@gmail.com>
2024-09-28 22:10:05 +0300

    Лабараторная работа №4

--- students/23K0365/23K0365-p04/src/main/java/ru/mirea/practice/s0000001/pw41/task2/TestPhone.java
+++ students/23K0365/23K0365-p04/src/main/java/ru/mirea/practice/s0000001/pw41/task2/TestPhone.java
@@ -1,18 +1,28 @@
 package ru.mirea.practice.s0000001.pw41.task2;
 
-public class TestPhone {
+public abstract class TestPhone {
     public static void main(String[] args) {
         Phone a = new Phone();
-        Phone b = new Phone(8987345,"Iphone");
-        System.out.println("Phone num: "+b.getNumber());
-        Phone c = new Phone(89871234,"Xiomi",999);
+        Phone b = new Phone(8987345, "Iphone");
+        System.out.println("Phone num: " + b.getNumber());
+        final Phone c = new Phone(89871234, "Xiomi", 999);
 
         a.recieveCall("Vanya");
-        b.recieveCall("Vanya",1234);
+        b.recieveCall("Vanya", 1234);
 
         System.out.println();
 
-        int[] arr= {1,12,123,1234,12345,123456,11234567,12345678,123456789};
+        int[] arr = {
+            1,
+            12,
+            123,
+            1234,
+            12345,
+            123456,
+            11234567,
+            12345678,
+            123456789
+        };
         c.sendMessage(arr);
     }
 }
