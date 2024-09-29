3f9bde3e3e0772ce79e164971acd51b047eb9a69
AraneWiFi <koblova2005@gmail.com>
2024-09-30 02:27:51 +0300

    Лабораторная работа №3

---
+++ students/23K0350/23K0350-p03/src/main/java/ru/mirea/practice/s23k0350/task1/Circle.java
@@ -0,0 +1,23 @@
+package ru.mirea.practice.s23k0350.task1;
+
+class Circle {
+    private Point center;
+    private float radius;
+
+    public Circle(Point center, float radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public float getLength() {
+        return 2 * (float) Math.PI * radius;
+    }
+}
