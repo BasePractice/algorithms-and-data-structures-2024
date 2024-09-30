8d7a12f2dcfc67560b52af4dd42fb6cf4d4ed72a
simas <simasomis@mail.ru>
2024-10-01 00:00:10 +0300

    lab 4 some complete

--- students/23K0351/23K0351-p04/src/main/java/ru/mirea/practice/s0000001/MainComputer.java
+++ students/23K0351/23K0351-p04/src/main/java/ru/mirea/practice/s0000001/MainComputer.java
@@ -4,7 +4,7 @@ public abstract class MainComputer {
     public static void main(String[] args) {
         Processor processor = new Processor("AMD Ryzen 7 7800X", 8, 4.2);
         Memory memory = new Memory("DDR5", 16);
-        Monitor monitor = new Monitor("1920x1080", 27.0);
+        Monitor monitor = new Monitor("1920x1080 ", 27.0);
 
         Computer computer = new Computer(Brand.ASUS, processor, memory, monitor);
 
