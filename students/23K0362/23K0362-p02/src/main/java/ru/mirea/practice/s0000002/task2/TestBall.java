0c706ceeb8d1674f644a5122fde1f89142d55b8c
Kr1sM0L <nuykindaniil2005@gmail.com>
2024-09-30 17:03:46 +0300

    Лабораторная работа #1-4.1

--- students/23K0362/23K0362-p02/src/main/java/ru/mirea/practice/s0000002/task2/TestBall.java
+++ students/23K0362/23K0362-p02/src/main/java/ru/mirea/practice/s0000002/task2/TestBall.java
@@ -3,14 +3,14 @@ package ru.mirea.practice.s0000002.task2;
 abstract class TestBall {
     public static void main(String[] args) {
         Ball aut1 = new Ball(15, 15);
-        System.out.println(aut1.toString());
+        System.out.println(aut1);
         aut1.setX(152.21);
         System.out.println(aut1.getX());
         aut1.setY(-1383.212);
         System.out.println(aut1.getY());
         aut1.setXY(0, 0);
-        System.out.println(aut1.toString());
+        System.out.println(aut1);
         aut1.move(-5, 7);
-        System.out.println(aut1.toString());
+        System.out.println(aut1);
     }
 }
