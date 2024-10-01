f6b778312f6532640629719e76e26a0f070e81b5
murtazinail <nail4704@gmail.com>
2024-10-01 15:06:22 +0300

    Лабораторная №1

---
+++ students/23F0011/23F0011-p01/src/main/java/ru/mirea/practice/s23f0011/Task5.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.s23f0011;
+
+public abstract class Task5 {
+    public static void main(String[] args) {
+        System.out.println("Задание 5 ");
+        System.out.println(args);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
