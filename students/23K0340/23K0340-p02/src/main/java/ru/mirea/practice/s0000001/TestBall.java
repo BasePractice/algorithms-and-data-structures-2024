a65f3b83406401eaf6fecf94c2c0571623c1357d
zula4z <zula4z2867@gmail.com>
2024-10-01 05:47:16 +0300

    Лабораторные №1-4.1

---
+++ students/23K0340/23K0340-p02/src/main/java/ru/mirea/practice/s0000001/TestBall.java
@@ -0,0 +1,13 @@
+package ru.mirea.practice.s0000001;
+
+public abstract class TestBall {
+    public static void main(String[] args) {
+        Ball ball = new Ball(0.0,0.0);
+        System.out.println(ball.getX());
+        System.out.println(ball.getY());
+        ball.setX(10.0);
+        ball.setY(5.0);
+        ball.move(10.0,10.0);
+        System.out.println(ball.toString());
+    }
+}
