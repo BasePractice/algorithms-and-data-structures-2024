1244b0d82e07d6d0e911be381071ef14412f0104
egorburov <gregorrogemimi@gmail.com>
2024-10-02 02:21:58 +0300

    Лабораторная работа №2

---
+++ students/23K0815/23K0815-p03/src/main/java/ru/mirea/practice/s0000001/task1/RandomArrayUtil.java
@@ -0,0 +1,56 @@
+package ru.mirea.practice.s0000001.task1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomArrayUtil {
+
+    private int size;
+
+    public RandomArrayUtil(int size) {
+        this.size = size;
+    }
+
+    public double[] generateArrayWithMathRandom() {
+        double[] array = new double[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = Math.random() * 100;
+        }
+        return array;
+    }
+
+    public double[] generateArrayWithRandom() {
+        Random random = new Random();
+        double[] array = new double[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextDouble() * 100;
+        }
+        return array;
+    }
+
+    public double[] sortArray(double[] array) {
+        Arrays.sort(array);
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        RandomArrayUtil util = new RandomArrayUtil(size);
+
+        System.out.println("Массив, сгенерированный с использованием Math.random():");
+        double[] mathRandomArray = util.generateArrayWithMathRandom();
+        System.out.println(Arrays.toString(mathRandomArray));
+
+        mathRandomArray = util.sortArray(mathRandomArray);
+        System.out.println("Отсортированный массив:");
+        System.out.println(Arrays.toString(mathRandomArray));
+
+        System.out.println("nМассив, сгенерированный с использованием класса Random:");
+        double[] randomArray = util.generateArrayWithRandom();
+        System.out.println(Arrays.toString(randomArray));
+
+        randomArray = util.sortArray(randomArray);
+        System.out.println("Отсортированный массив:");
+        System.out.println(Arrays.toString(randomArray));
+    }
+}
---
+++ students/23K0815/23K0815-p03/src/main/java/ru/mirea/practice/s0000001/task2/Main.java
@@ -0,0 +1,32 @@
+package ru.mirea.practice.s0000001.task2;
+
+public final class Main {
+
+    private Main() {
+        throw new UnsupportedOperationException("Utility class should not be instantiated");
+    }
+
+    public static void main(String[] args) {
+        Double doubleObject1 = Double.valueOf(3.14);
+        Double doubleObject2 = Double.valueOf("2.71");
+
+        String doubleString = "1.618";
+        double primitiveDouble = Double.parseDouble(doubleString);
+
+        int intValue = doubleObject1.intValue();
+        long longValue = doubleObject1.longValue();
+        float floatValue = doubleObject1.floatValue();
+        byte byteValue = doubleObject1.byteValue();
+
+        System.out.println("Объект Double 1: " + doubleObject1);
+        System.out.println("Объект Double 2: " + doubleObject2);
+        System.out.println("Преобразованное значение типа String к double: " + primitiveDouble);
+        System.out.println("Примитивный int из Double: " + intValue);
+        System.out.println("Примитивный long из Double: " + longValue);
+        System.out.println("Примитивный float из Double: " + floatValue);
+        System.out.println("Примитивный byte из Double: " + byteValue);
+
+        String d = Double.toString(3.14);
+        System.out.println("Литерал типа double в строке: " + d);
+    }
+}
