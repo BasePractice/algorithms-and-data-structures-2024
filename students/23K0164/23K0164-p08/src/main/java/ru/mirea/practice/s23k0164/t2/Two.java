a957146427432fbcd0f2cc821da9521115d20d23
nastya <milovanova-aa@yandex.ru>
2024-10-02 22:39:27 +0300

    Лаба 8

---
+++ students/23K0164/23K0164-p08/src/main/java/ru/mirea/practice/s23k0164/t2/Two.java
@@ -0,0 +1,22 @@
+package ru.mirea.practice.s23k0164.t2;
+
+public final class Two {
+
+    private Two() {
+
+    }
+
+    private static void outputOfNumbers(int currentNumber, int totalCount) {
+        if (totalCount <= 0) {
+            return;
+        }
+        System.out.print(currentNumber + " ");
+        totalCount--;
+        outputOfNumbers(currentNumber + 1, totalCount);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        outputOfNumbers(1, n);
+    }
+}
