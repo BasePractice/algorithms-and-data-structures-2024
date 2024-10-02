ed69522dbc3ac1a46e51fefe505f25da2a3e4343
nastya <milovanova-aa@yandex.ru>
2024-10-02 22:32:17 +0300

    Лаба 7

---
+++ students/23K0164/23K0164-p07/src/main/java/ru/mirea/practice/s23k0164/t2/MathCalculable.java
@@ -0,0 +1,9 @@
+package ru.mirea.practice.s23k0164.t2;
+
+public interface MathCalculable {
+    double PI = 3.141592653589793;
+
+    double power(double base, double degree);
+
+    double modulus(double realPart, double imaginaryPart);
+}
