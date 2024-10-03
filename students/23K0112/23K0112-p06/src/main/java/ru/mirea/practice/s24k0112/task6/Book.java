0c2d08f0ae5204768bede3b677a650c7e9d3a4a5
or0ka <m.gati@yandex.ru>
2024-10-03 23:47:20 +0300

    Практические №5-10 исправленные

--- students/23K0112/23K0112-p06/src/main/java/ru/mirea/practice/s24k0112/task6/Book.java
+++ students/23K0112/23K0112-p06/src/main/java/ru/mirea/practice/s24k0112/task6/Book.java
@@ -1,7 +1,5 @@
 package ru.mirea.practice.s24k0112.task6;
 
-import ru.mirea.practice.s25k0112.task6.Printable;
-
 public class Book implements Printable {
     public String name;
     public String author;
--- students/23K0112/23K0112-p06/src/main/java/ru/mirea/practice/s24k0112/task6/Test.java
+++ students/23K0112/23K0112-p06/src/main/java/ru/mirea/practice/s24k0112/task6/Test.java
@@ -1,15 +1,13 @@
 package ru.mirea.practice.s24k0112.task6;
 
-import ru.mirea.practice.s25k0112.task6.Book;
-import ru.mirea.practice.s25k0112.task6.Printable;
-
 public final class Test {
     private Test() {
 
     }
 
     public static void main(String[] args) {
-        ru.mirea.practice.s25k0112.task6.Printable[] books = {new ru.mirea.practice.s25k0112.task6.Book("Lord of Rings", "Tolkien"), new Book("War and peace", "Tolstoi")};
+        Printable[] books = {new Book("Lord of Rings", "Tolkien"), new Book("War and peace",
+                "Tolstoi")};
         for (Printable book : books) {
             book.print();
         }
