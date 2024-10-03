70ad535d7af8bdc1d59722cdfdc6ade7d96b6bb5
DNLdmitrylavrischev <di.dima.ru888@mail.ru>
2024-10-03 18:25:47 +0300

    Лабораторная работа №1

---
+++ students/23K0355/23K0355-p01/src/main/java/ru/mirea/practice/s0000001/Main.java
@@ -0,0 +1,31 @@
+package ru.mirea.practice.s0000001;
+
+import java.util.Scanner;
+
+public abstract class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Введите количество элементов массива: ");
+            int n = sc.nextInt();
+
+            int[] nums = new int[n];
+
+            System.out.println("Введите элементы массива:");
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+
+            int sum = 0;
+            double avg;
+
+            for (int num : nums) {
+                sum += num;
+            }
+
+            avg = (double) sum / nums.length;
+
+            System.out.println("Сумма элементов массива: " + sum);
+            System.out.println("Среднее арифметическое элементов массива: " + avg);
+        }
+    }
+}
