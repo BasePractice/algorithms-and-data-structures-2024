3808202e6c960797c4e331605a0708824193cd3a
egorburov <gregorrogemimi@gmail.com>
2024-10-03 19:01:04 +0300

    Лабораторная работа №2

---
+++ students/23K0815/23K0815-p02_1/src/main/java/ru/mirea/practice/s0000001/task1/CardGame.java
@@ -0,0 +1,37 @@
+package ru.mirea.practice.s0000001.task1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class CardGame {
+    public static void main(String[] args) {
+        String[] type = {"Пики", "Черви", "Крести", "Бубны"};
+        String[] level = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Королева", "Король", "Туз"};
+
+        int n = 5;
+
+        List<Cards> deck = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                deck.add(new Cards(type[i], level[j]));
+            }
+        }
+
+        Collections.shuffle(deck);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Player " + (i + 1) + ": ");
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print(deck.remove(0).getSuite() + " | " + deck.remove(0).getRank());
+
+                if (j < 4) {
+                    System.out.print(", ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ students/23K0815/23K0815-p02_1/src/main/java/ru/mirea/practice/s0000001/task1/Cards.java
@@ -0,0 +1,27 @@
+package ru.mirea.practice.s0000001.task1;
+
+public class Cards {    private String type;
+    private String level;
+
+
+    public Cards(String type, String level) {
+        this.type = type;
+        this.level = level;
+    }
+
+    public String getSuite() {
+        return type;
+    }
+
+    public void setSuite(String suite) {
+        this.type = suite;
+    }
+
+    public String getRank() {
+        return level;
+    }
+
+    public void setRanks(String rank) {
+        this.level = rank;
+    }
+}
