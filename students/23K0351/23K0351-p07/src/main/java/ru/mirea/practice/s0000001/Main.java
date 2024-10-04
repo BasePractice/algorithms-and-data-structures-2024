8845a1d80a43f1a71d8ebdaf991246765d9c5ff3
simas <simasomis@mail.ru>
2024-10-04 11:47:09 +0300

    lab 7 all complete

--- students/23K0351/23K0351-p07/src/main/java/ru/mirea/practice/s0000001/Main.java
+++ students/23K0351/23K0351-p07/src/main/java/ru/mirea/practice/s0000001/Main.java
@@ -5,7 +5,7 @@ public abstract class Main {
         MathCalculable mc = new MathFunc();
 
         System.out.printf("2^4 = %f\n", mc.power(2, 4));
-        System.out.printf("Modulus of (3 + 4i) = %f\n", mc.modulus(3, 4));
+        System.out.printf("Modulus of  (3 + 4i) = %f\n", mc.modulus(3, 4));
 
         MathFunc mathFunc = new MathFunc();
         System.out.printf("Circle with radius 5 = %f\n", mathFunc.circle(5));
