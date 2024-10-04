6c9c76508f0cb201735f60a14dc6817df4eab3cb
egorburov <gregorrogemimi@gmail.com>
2024-10-04 13:29:30 +0300

    Лабораторная работа №2!

--- students/23K0815/23K0815-p02_1/src/main/java/ru/mirea/practice/s0000001/task1/WordCountUtil.java
+++ students/23K0815/23K0815-p02_1/src/main/java/ru/mirea/practice/s0000001/task1/WordCountUtil.java
@@ -8,14 +8,13 @@ public final class WordCountUtil {
 
     private static final Logger logger = Logger.getLogger(WordCountUtil.class.getName());
 
-    // Private constructor to prevent instantiation
     private WordCountUtil() {
         throw new UnsupportedOperationException("Utility class should not be instantiated");
     }
 
     public static int countWords(String sentence) {
         if (sentence == null || sentence.isEmpty()) {
-            return 0; // Return 0 for null or empty strings
+            return 0;
         }
 
         int counter = 0;
@@ -26,10 +25,10 @@ public final class WordCountUtil {
             if (currentChar != ' ') {
                 if (flag) {
                     counter++;
-                    flag = false; // Next characters are not a space
+                    flag = false;
                 }
             } else {
-                flag = true; // Found a space, next character will be checked
+                flag = true;
             }
         }
 
@@ -39,7 +38,6 @@ public final class WordCountUtil {
     public static void main(String[] args) {
         System.out.println("Enter string:");
 
-        // Use try-with-resources to ensure automatic resource management
         try (Scanner scanner = new Scanner(System.in)) {
             String sentence = scanner.nextLine();
             int wordCount = countWords(sentence);
