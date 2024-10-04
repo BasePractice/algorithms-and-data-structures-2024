e933dcf9e10a246321cca288a0f84863612fb21c
Cyxapuk228777 <shushpanov.vsevolod@gmail.com>
2024-10-04 20:32:39 +0300

    Практическая 6

--- students/23K0375/23K0375-p06/src/main/java/ru/mirea/practice/s0000001/task11/KelvCon.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.practice.s0000001.task11;
-
-public class KelvCon implements Convertable {
-    @Override
-    public double convert(double celsius) {
-        return celsius + 273.15;
-    }
-}
