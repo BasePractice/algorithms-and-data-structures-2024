b03162bf61f568eeddb85aa1c81cfc3c41ab1e9a
Florist01 <sembroone@gmail.com>
2024-10-04 13:25:06 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pom.xml

--- students/23K1167/23K1167-p01/src/main/java/ru/mirea/practice/s23k1167/Main.java
+++ students/23K1167/23K1167-p01/src/main/java/ru/mirea/practice/s23k1167/Main.java
@@ -1,12 +1,23 @@
-package ru.mirea.practice.s0000001;
+package ru.mirea.practice.s23k1167;
 
-public final class Main {
-
-    private Main() {
-
-    }
-
+public abstract class Main {
     public static void main(String[] args) {
-        System.out.println("первая практическая работа!");
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+
+        int totalSum = 0;
+
+
+        for (int num : numbers) {
+            totalSum += num;
+        }
+
+
+        double average = (double) totalSum / numbers.length;
+
+
+        System.out.println(+ totalSum);
+        System.out.println(+ average);
     }
 }
