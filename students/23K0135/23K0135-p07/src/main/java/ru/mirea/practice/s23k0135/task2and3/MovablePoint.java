158fd28e49f4e1c4f37a12402abf7c4eebf3d061
Anna Znaydenova <znaydann@gmail.com>
2024-10-11 15:34:27 +0300

    Лабораторная №1-10

---
+++ students/23K0135/23K0135-p07/src/main/java/ru/mirea/practice/s23k0135/task2and3/MovablePoint.java
@@ -0,0 +1,44 @@
+package ru.mirea.practice.s23k0135.task2and3;
+
+public class MovablePoint implements Movable {
+    private final double xspeed;
+    private final double yspeed;
+    private double x;
+    private double y;
+
+    public MovablePoint(double x, double y, double xspeed, double yspeed) {
+        this.x = x;
+        this.y = y;
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+    }
+
+    public void moveUp() {
+        this.y += yspeed;
+    }
+
+    public void moveDown() {
+        this.y -= yspeed;
+    }
+
+    public void moveLeft() {
+        this.x -= xspeed;
+    }
+
+    public void moveRight() {
+        this.x += xspeed;
+    }
+
+    public double getxspeed() {
+        return xspeed;
+    }
+
+    public double getyspeed() {
+        return yspeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" + "x=" + x + ", y=" + y + ", xspeed=" + xspeed + ", yspeed=" + yspeed + '}';
+    }
+}
