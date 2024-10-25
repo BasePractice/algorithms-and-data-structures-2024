2c3a0eb70aeae21ac0e28c7cee619828be4330f4
Timmmmofey <153191260+Timmmmofey@users.noreply.github.com>
2024-10-25 13:38:44 +0300

    Лабораторная №12 V2

--- students/23K1302/23K1302-p12/src/main/java/ru/mirea/practice/s0000001/Ex2.java
+++ students/23K1302/23K1302-p12/src/main/java/ru/mirea/practice/s0000001/Ex2.java
@@ -4,10 +4,14 @@ import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Objects;
 
 public class Ex2 extends JFrame {
 
-    public Ex2(String imagePath) {
+    public Ex2(URL imagePath) {
         ImageIcon image = new ImageIcon(imagePath);
         if (image.getIconWidth() == -1) {
             System.out.println("Ошибка: изображение не загружено.");
@@ -20,15 +24,16 @@ public class Ex2 extends JFrame {
         }
     }
 
-    public static void main(String[] args) {
-        String imagePath;
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException {
+        URL imagePath;
         if (args.length < 1) {
             System.out.println("No command-line argument provided. Using default image path.");
-            imagePath = "resources/img.png"; // Здесь я написал "Заглушку" чтобы не нужно было писать путь в аргументах командной строки
+            imagePath = Objects.requireNonNull(Ex2.class.getResource("/img.png")).toURI().toURL();
+            // Здесь я написал "Заглушку" чтобы не нужно было писать путь в аргументах командной строки
         } else {
-            imagePath = args[0];
+            imagePath = new URL("file://" + args[0]);
         }
-        // Но по каким-то одному только богу известным причинам программа не может открыть эту картинку
+
         SwingUtilities.invokeLater(() -> {
             Ex2 frame = new Ex2(imagePath);
             frame.setVisible(true);
