930b62b78578e8a0cdf0c22b954b1632634f026f
Romkarus <romkarusmail@gmail.com>
2024-11-06 21:11:39 +0300

    20 pr

---
+++ students/23K0215/23K0215-p19/src/main/java/ru/mirea/practice/s23k0215/task2/EmptyStringException.java
@@ -0,0 +1,8 @@
+package ru.mirea.practice.s23k0215.task2;
+
+public class EmptyStringException extends Exception {
+    public EmptyStringException(String message) {
+        super(message);
+    }
+}
+
