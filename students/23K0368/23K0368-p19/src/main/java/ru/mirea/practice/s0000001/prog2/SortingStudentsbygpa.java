6780afe9c60530f96da923a0736051958d7e3e3a
AndreySkrypov <a.skrypov2005@gmail.com>
2024-11-06 20:33:40 +0300

    Лабораторные работы 11-20

--- students/23K0368/23K0368-p19/src/main/java/ru/mirea/practice/s0000001/prog2/SortingStudentsbygpa.java
+++ students/23K0368/23K0368-p19/src/main/java/ru/mirea/practice/s0000001/prog2/SortingStudentsbygpa.java
@@ -3,27 +3,30 @@ package ru.mirea.practice.s0000001.prog2;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public abstract class SortingStudentsbygpa {
     public static <T extends Comparable<T>> List<T> qsort(List<T> students) {
         if (students.size() <= 1) {
             return students;
         }
-        T tmp = students.getFirst();
-        List<T> prList = new ArrayList<>();
-        List<T> lvList = new ArrayList<>();
+
+        T pivot = students.get(0);  // Опорный элемент
+        List<T> less = new ArrayList<>();
+        List<T> greater = new ArrayList<>();
+
+        // Разделение элементов на две группы
         for (int i = 1; i < students.size(); i++) {
-            if (students.get(i).compareTo(tmp) <= 0) {
-                prList.add(students.get(i));
-            }
-            if (students.get(i).compareTo(tmp) >= 0) {
-                lvList.add(students.get(i));
+            if (students.get(i).compareTo(pivot) < 0) {
+                less.add(students.get(i));
+            } else {
+                greater.add(students.get(i));
             }
         }
-        List<T> result = new ArrayList<>();
-        result.addAll(qsort(prList));
-        result.add(tmp);
-        result.addAll(qsort(lvList));
+
+        // Рекурсивная сортировка подсписков и объединение результатов
+        List<T> result = new ArrayList<>(qsort(less));
+        result.add(pivot);
+        result.addAll(qsort(greater));
+
         return result;
     }
 }
