f04fa127aa153b4f9b101e1d3181195c13d3f755
Florist01 <sembroone@gmail.com>
2024-11-08 22:18:05 +0300

    123

---
+++ students/23K1167/23K1167-p13/src/main/java/ru/mirea/practice/s23k1167/Task1.java
@@ -0,0 +1,13 @@
+package ru.mirea.practice.s23k1167;
+
+/**
+ * Напишите метод, который принимает в качестве параметра любую
+ * строку, например “I like Java!!!”.
+ */
+
+public abstract class Task1 {
+    static void method(String param) {
+        System.out.println("Получена стока:");
+        System.out.println(param);
+    }
+}
---
+++ students/23K1167/23K1167-p18/src/main/java/ru/mirea/practice/s23k1167/t2/Two.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s23k1167.t2;
+
+
+public final class Two {
+    private Two() {
+
+    }
+
+    public static void main(String[] args) {
+        Exception2.exceptionDemo();
+    }
+}
