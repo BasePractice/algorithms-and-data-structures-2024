37c2f2ba6eeaf90326c77b2f0bc1100392eeea88
Timmmmofey <153191260+Timmmmofey@users.noreply.github.com>
2024-11-08 18:35:14 +0300

    Лабораторная 17-20 V2

--- students/23K1302/23K1302-p17/src/main/java/ru/mirea/practice/s0000001/Ex1.java
+++ students/23K1302/23K1302-p17/src/main/java/ru/mirea/practice/s0000001/Ex1.java
@@ -2,7 +2,11 @@ package ru.mirea.practice.s0000001;
 
 import java.util.Scanner;
 
-public class Ex1 {
+public final class Ex1 {
+
+    private Ex1() {
+        // fgvbdfsbhlg
+    }
 
     public static String funk(boolean a) {
         if (a) {
@@ -14,29 +18,45 @@ public class Ex1 {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        Scanner scanner = new Scanner(System.in);
-        int choice;
 
-        do {
-            System.out.println("\nМеню:");
-            System.out.println("1. Добавить элемент");
-            System.out.println("2. Удалить элемент");
-            System.out.println("3. Вывести список");
-            System.out.println("4. Очистить список");
-            System.out.println("5. Проверить, пуст ли список");
-            System.out.println("0. Выход");
-            System.out.print("Выберите опцию: ");
-            choice = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int choice;
 
-            switch (choice) {
-                case 1 -> list.addElement();
-                case 2 -> list.removeElement();
-                case 3 -> list.displayElements();
-                case 4 -> list.clearList();
-                case 5 -> System.out.println(funk(list.isEmpty()));
-                case 0 -> System.out.println("Выход из программы.");
-                default -> System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
-            }
-        } while (choice != 0);
+            do {
+                System.out.println("\nМеню:");
+                System.out.println("1. Добавить элемент");
+                System.out.println("2. Удалить элемент");
+                System.out.println("3. Вывести список");
+                System.out.println("4. Очистить список");
+                System.out.println("5. Проверить, пуст ли список");
+                System.out.println("0. Выход");
+                System.out.print("Выберите опцию: ");
+                choice = scanner.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        list.addElement();
+                        break;
+                    case 2:
+                        list.removeElement();
+                        break;
+                    case 3:
+                        list.displayElements();
+                        break;
+                    case 4:
+                        list.clearList();
+                        break;
+                    case 5:
+                        System.out.println(funk(list.isEmpty()));
+                        break;
+                    case 0:
+                        System.out.println("Выход из программы.");
+                        break;
+                    default:
+                        System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
+                        break;
+                }
+            } while (choice != 0);
+        }
     }
 }
