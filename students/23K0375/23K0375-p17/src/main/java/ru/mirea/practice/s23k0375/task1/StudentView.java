fde92c8975941603395497ae3522796ead8f3363
Cyxapuk228777 <shushpanov.vsevolod@gmail.com>
2024-11-08 23:43:52 +0300

    Практические 17 и 20

---
+++ students/23K0375/23K0375-p17/src/main/java/ru/mirea/practice/s23k0375/task1/StudentView.java
@@ -0,0 +1,9 @@
+package ru.mirea.practice.s23k0375.task1;
+
+public class StudentView {
+    public void printstudentdetails(String rollNo,String name) {
+        System.out.println("Student Details: ");
+        System.out.println("Name: " + name);
+        System.out.println("rollNo: " + rollNo);
+    }
+}
---
+++ students/23K0375/23K0375-p17/src/main/java/ru/mirea/practice/s23k0375/task2/EmployeeView.java
@@ -0,0 +1,9 @@
+package ru.mirea.practice.s23k0375.task2;
+
+public class EmployeeView {
+    public void printemployeedetails(int salary,String name) {
+        System.out.println("Employee Details: ");
+        System.out.println("Name: " + name);
+        System.out.println("salary: " + salary);
+    }
+}
