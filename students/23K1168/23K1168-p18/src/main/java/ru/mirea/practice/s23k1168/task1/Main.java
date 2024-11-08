b16949919f9e86bf4132d6b91b69a0c483f5220c
AlvinaKadieva <abdylova00@gmail.com>
2024-11-08 19:45:22 +0300

    Лабороторные работы 11-20

---
+++ students/23K1168/23K1168-p18/src/main/java/ru/mirea/practice/s23k1168/task1/Main.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.s23k1168.task1;
+
+public final class Main {
+    private Main() {
+
+    }
+
+    public static void main(String[] args) {
+        Exception.exceptionDemo();
+    }
+}
