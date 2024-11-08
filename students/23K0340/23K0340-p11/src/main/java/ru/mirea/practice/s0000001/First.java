ca4c11ea9309bf2707527bb262a6bf630fbe1b37
zula4z <zula4z2867@gmail.com>
2024-11-08 14:04:08 +0300

    Лабораторные №11-20

---
+++ students/23K0340/23K0340-p11/src/main/java/ru/mirea/practice/s0000001/First.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s0000001;
+
+import java.util.Date;
+
+public abstract class First {
+    public static void main(String[] args) {
+        Date dated = new Date();
+        System.out.println("Аниськин");
+        System.out.println("Fri Sep 06 09:00:00 GMT+03:00 2024");
+        System.out.println(dated.toString());
+    }
+}
