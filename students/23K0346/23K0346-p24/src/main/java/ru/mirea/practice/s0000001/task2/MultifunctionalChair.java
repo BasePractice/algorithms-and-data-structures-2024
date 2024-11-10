24dfa2484ec6a94039d84d49a383bde39722a685
marina-devel <marina_030405@mail.ru>
2024-11-11 02:42:32 +0300

    Лабораторная работа №24

---
+++ students/23K0346/23K0346-p24/src/main/java/ru/mirea/practice/s0000001/task2/MultifunctionalChair.java
@@ -0,0 +1,9 @@
+package ru.mirea.practice.s0000001.task2;
+
+public class MultifunctionalChair implements Chair {
+    @Override
+    public void sit() {
+        System.out.println("Вы сидите на Многофункциональном стуле, который может трансформироваться.");
+    }
+}
+
---
+++ students/23K0346/23K0346-p24/src/main/java/ru/mirea/practice/s0000001/task2/VictorianChair.java
@@ -0,0 +1,9 @@
+package ru.mirea.practice.s0000001.task2;
+
+public class VictorianChair implements Chair {
+    @Override
+    public void sit() {
+        System.out.println("Вы сидите на Викторианском стуле.");
+    }
+}
+
