2a61bd2502e97b43012b46d893c14387bd566e41
Anna Znaydenova <znaydann@gmail.com>
2024-11-16 22:27:19 +0300

    Лабораторная №22

---
+++ students/23K0135/23K0135-p22/src/main/java/ru/mirea/practice/s23k0135/task2/CalculatorController.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.s23k0135.task2;
+
+class CalculatorController {
+    CalculatorModel model;
+    CalculatorView view;
+
+    public CalculatorController(CalculatorModel model, CalculatorView view) {
+        this.model = model;
+        this.view = view;
+    }
+}
