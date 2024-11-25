7e8e2fb2a4356d0c1709bd0762a07b80191f36c4
Elizaveta Knyazeva <Liza.knyazeva62@gmail.com>
2024-11-26 00:01:07 +0300

    Лабораторная №30 части 1 и 2

---
+++ students/23K0143/23K0143-p30_2/src/main/java/ru/mirea/practice/s23k0143/Node.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s23k0143;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
