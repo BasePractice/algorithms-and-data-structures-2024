ecfacfbf94863f605f6485ff5261eb0f083eba38
MeowWewLord <Sysoenko2005@mail.ru>
2024-12-10 20:06:46 +0300

    Внезапные запчасти (работа 30 часть 2)

---
+++ students/23K0823/23K0823-p30/src/main/java/ru/mirea/practice/s23k0823/task1/task1/task2/part2/Node.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s23k0823.task1.task1.task2.part2;
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
