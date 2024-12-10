84e1f55c49ee9862d5ede06419d37413f8e1d4d1
kaatrina <khan.10a@yandex.ru>
2024-12-10 15:29:10 +0300

    Лабараторные №21-32

---
+++ students/23K0202/23K0202-p30/src/main/java/ru/mirea/practice/s0000001/Nodee.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s0000001;
+
+public class Nodee {
+    int data;
+    Nodee left;
+    Nodee right;
+
+    public Nodee(int item) {
+        data = item;
+        left = right = null;
+    }
+}
