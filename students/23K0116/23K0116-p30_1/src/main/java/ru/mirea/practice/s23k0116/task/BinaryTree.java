620181596549d38a9d8aeefb1a17131736e39c24
Ra1nyShe4ka <ra1n4ik.gregori@gmail.com>
2024-12-10 20:28:23 +0300

    Лабораторные работы 30-32

---
+++ students/23K0116/23K0116-p30_1/src/main/java/ru/mirea/practice/s23k0116/task/BinaryTree.java
@@ -0,0 +1,95 @@
+package ru.mirea.practice.s23k0116.task;
+
+public class BinaryTree {
+    Node root;
+
+    int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int heighLeft = height(node.left);
+        int heighRight = height(node.right);
+        return Math.max(heighLeft, heighRight) + 1;
+    }
+
+    boolean lookup(Node node, int target) {
+        if (node == null) {
+            return false;
+        }
+        if (node.data == target) {
+            return true;
+        }
+        if (target < node.data) {
+            return lookup(node.left, target);
+        } else {
+            return lookup(node.right, target);
+        }
+    }
+
+    int getMaxWidth(Node root) {
+        int h = this.height(this.root);
+        int maxWidth = 0;
+        for (int i = 1; i <= h; i++) {
+            int w = getWidth(this.root, i);
+            if (w > maxWidth) {
+                maxWidth = w;
+            }
+        }
+        return maxWidth;
+    }
+
+    int getWidth(Node node, int level) {
+        if (node == null) {
+            return 0;
+        }
+        if (level == 1) {
+            return 1;
+        }
+        return getWidth(node.left, level - 1) + getWidth(node.right, level - 1);
+    }
+
+    int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return size(node.left) + 1 + size(node.right);
+    }
+
+    boolean sameTree(Node a, Node b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a != null && b != null) {
+            return a.data == b.data
+                && sameTree(a.left, b.left)
+                && sameTree(a.right, b.right);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(10);
+        tree.root.left = new Node(5);
+        tree.root.right = new Node(15);
+        tree.root.left.left = new Node(2);
+        tree.root.left.right = new Node(7);
+
+        System.out.println("Высота дерева: " + tree.height(tree.root));
+        System.out.println("Размер дерева: " + tree.size(tree.root));
+        System.out.println("Максимальная ширина дерева: " + tree.getMaxWidth(tree.root));
+
+        int target1 = 2;
+        System.out.println("Поиск " + target1 + ": " + tree.lookup(tree.root, target1));
+        int target2 = 4;
+        System.out.println("Поиск " + target2 + ": " + tree.lookup(tree.root, target2));
+
+        BinaryTree tree2 = new BinaryTree();
+        tree2.root = new Node(7);
+        tree2.root.left = new Node(1);
+        tree2.root.right = new Node(4);
+
+        System.out.println("Являются ли эти два дерева одинаковыми? " + tree.sameTree(tree.root, tree2.root));
+    }
+}
+
---
+++ students/23K0116/23K0116-p31/src/main/java/ru/mirea/practice/s23k0116/task1/Node.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.s23k0116.task1;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int d) {
+        this.data = d;
+        left = right = null;
+    }
+}
