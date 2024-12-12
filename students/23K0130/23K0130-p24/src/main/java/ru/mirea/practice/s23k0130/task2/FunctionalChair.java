751b081c756f7f073be57f5630c21471c07ca8c8
dendrokton <eroff.anton@gmail.com>
2024-12-12 21:35:26 +0300

    Лабораторная 21-32

---
+++ students/23K0130/23K0130-p24/src/main/java/ru/mirea/practice/s23k0130/task2/FunctionalChair.java
@@ -0,0 +1,8 @@
+package ru.mirea.practice.s23k0130.task2;
+
+public class FunctionalChair implements Chair {
+    @Override
+    public void sit() {
+        System.out.println("Сидим на функциональном стуле");
+    }
+}
