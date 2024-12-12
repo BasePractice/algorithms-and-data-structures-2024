6884bc2a1dda99b58648c122065bc2573f8f048a
juliavlv <bocharova-2004@bk.ru>
2024-12-12 13:07:17 +0300

    Практические 20-31

---
+++ students/23K0342/23K0342-p23/src/main/java/ru/mirea/practice/s0000001/task1/ArrayQueueAdT.java
@@ -0,0 +1,56 @@
+package ru.mirea.practice.s0000001.task1;
+
+public class ArrayQueueAdT {
+    private Object[] elements;
+    private int size;
+    private int front;
+    private int rear;
+
+    public ArrayQueueAdT() {
+        elements = new Object[10];
+        size = 0;
+        front = 0;
+        rear = 0;
+    }
+
+    public void enqueue(Object element) {
+        if (size == elements.length) {
+            throw new IllegalStateException("Queue is full");
+        }
+        elements[rear] = element;
+        rear = (rear + 1) % elements.length;
+        size++;
+    }
+
+    public Object element() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return elements[front];
+    }
+
+    public Object dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        Object result = elements[front];
+        front = (front + 1) % elements.length;
+        size--;
+        return result;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        size = 0;
+        front = 0;
+        rear = 0;
+    }
+}
+
