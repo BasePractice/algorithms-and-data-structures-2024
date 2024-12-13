38d14e28247ead59642b39b83bec3a4067fd7477
Florist01 <sembroone@gmail.com>
2024-12-13 23:04:53 +0300

    123

--- students/23K1167/23K1167-p30/src/main/java/ru/mirea/practice/s23k1167/task1/task1/task2/task2/HuffmanComparator.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.practice.s23k1167.task1.task1.task2.task2;
-
-import java.util.Comparator;
-
-class HuffmanComparator implements Comparator<HuffmanNode> {
-    public int compare(HuffmanNode x, HuffmanNode y) {
-        return x.freq - y.freq;
-    }
-}
