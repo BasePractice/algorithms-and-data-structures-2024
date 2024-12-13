53a51e2337822b422e487bfdbde6e788fac7c325
Lenovo <samsungmikle@gmail.com>
2024-12-13 22:37:54 +0300

    Lab30-31

---
+++ students/23K0565/23K0565-p30-1/src/main/java/ru/mirea/practice/s0000001/n1/HuffmanN.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.s0000001.n1;
+
+public class HuffmanN {
+    int freq;
+    HuffmanN left;
+    HuffmanN right;
+
+    public HuffmanN(int freq) {
+        this.freq = freq;
+    }
+}
